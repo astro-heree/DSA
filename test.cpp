d6dc44251a2b8388ba694e3a12d2b5b3ae8ace7d
astro-heree <aman.kumar.astro@gmail.com>
2024-05-12 19:42:56 +0530

    Testing

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define fast_io() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL) 
+using namespace std;
+#define ll long long
+#define ss second
+#define ff first
+int MOD  = 1e9+7;
+
+int n
+
+void solveAnswer(){
+    
+}
+int main() {
+    fast_io();
+    int t = 1;
+    //cin>>t; 
+    while(t--){
+        solveAnswer();
+    }
+    
+    return 0;
+}
